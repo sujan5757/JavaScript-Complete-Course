b0959b4e0fc1c917d921d6b39ba02048755e29b2
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-02-29 10:34:14 +0530

    Create letconstvar.js

---
+++ letconstvar.js
@@ -0,0 +1,16 @@
+//let keyword:
+let name = "Tony stark";
+let age = 24;
+let totalPrice = 1000;
+let ab;
+console.log(name, age, totalPrice, ab);
+// in intial days we  used var keyword we don't use var keyword after 2015  we got ES6 ecma script 6 in that we got let and const we can redeclare also variable
+
+
+// var name = "Tony stark";
+// var age = 24;
+// var totalPrice = 1000;
+
+
+// var age = 24;
+// var age = 59;
