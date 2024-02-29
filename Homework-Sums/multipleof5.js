fd21bde9995278ca439de384b6fe8ba94702584f
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-02-29 10:46:05 +0530

    Create multipleof5.js

---
+++ Homework-Sums/multipleof5.js
@@ -0,0 +1,9 @@
+//get user to input a number using prompt("enter a number"). check tyhe number is multiple of 5 ]
+
+
+let a = prompt("enter a nnumber");
+if (a % 5 == 0) {
+    console.log("Number is multiple of 5");
+} else {
+    console.log("Number is not multiple of 5");
+}
