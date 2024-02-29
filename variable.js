4b28cbf67bb874d662a03798c8ff90f99243b687
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-02-29 10:33:16 +0530

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,32 @@
+//Variable basics
+fullName = "Tony starck"; //name
+console.log(fullName);
+age = 24;
+price = 99.99;
+x = null;
+y = undefined;
+console.log(price);
+console.log(age);
+console.log(x);
+console.log(y);
+//boolean variables
+
+
+isFollow = false;
+isFollows = true;
+
+
+console.log(isFollow);
+console.log(isFollows);
+//we used = this symbol is called assignment operator
+/*
+Output : 
+Tony starck
+first.js:7 99.99
+first.js:8 24
+first.js:9 null
+first.js:10 undefined
+False
+True
+*/
+
