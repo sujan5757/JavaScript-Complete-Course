4ba7fd1832b1f52c30eccca1cdeafdee27eb6bda
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:59:23 +0530

    Create second.js

---
+++ DocumentObjectModel/QuerySelector/second.js
@@ -0,0 +1,16 @@
+let div = document.querySelectorAll(".box");
+console.log(div);
+div[0].innerText = "new array";
+
+
+
+
+let i = 0;
+for (divs of div) {
+
+
+    console.log(`array ement mal ${i}`);
+
+
+    i++;
+}
