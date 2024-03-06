9eb7c58ab46b6366881ed82f839cff98b93dc871
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:17:36 +0530

    Create datatypes.js

---
+++ Basics/datatypes.js
@@ -0,0 +1,23 @@
+//Data types : 
+//Primitive data types  - 7  {Number,String,Boolean , Undefined , null, Bigint, Symbol} 
+
+{
+    let age = 24;
+    console.log("Age: " + age);
+} {
+    let name = "Sujan";
+    console.log("Name: " + name);
+} {
+    isFollw = true;
+    console.log("Follow: " + isFollw);
+} {
+    let x;
+    console.log("X: " + x);
+}
+let x = BigInt("123");//BigInt/function
+let y = Symbol("Hello");//symbol/function
+console.log(x, y);
+/*typeof  age > numbers
+typeof  name > String
+typeof  isFollow > boolean
+*/
