f781ad8ff6577dbef172662825399c62fd55a522
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:55:54 +0530

    Create DomManipulation.js

---
+++ DocumentObjectModel/DomManipulation.js
@@ -0,0 +1,22 @@
+/*DOM MANIPULATION : 
+Selecting the with id -let threading =document.getElementById(“header”);//it will retur any value
+console.log(heading);  by using #
+Selecting the with class -document.getElementBy*/
+//Document object model
+
+
+// <style> tag connects html with css
+
+
+alert("hello");
+//se;ecting with the id;
+let heading = document.getElementById("heading");
+console.dir(heading);
+//selecting with class
+let headings = document.getElementsByClassName("heading");
+console.dir(headings);
+console.log(heading);
+//selecting with tag
+
+
+document.getElementBytagName("heading").;
