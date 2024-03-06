7addc93a4e8ae8a48b8bfc964ffc9f04d73d736a
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:42:12 +0530

    Create CreatingArray.js

---
+++ array/CreatingArray.js
@@ -0,0 +1,13 @@
+//arrays in javascript arrays' - collection of items
+
+
+//create a new array
+
+
+let newArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(newArray);
+
+
+let marks = [12, 87, 67, 67, 89]
+console.log(marks);
+console.log(marks.length); //length of the array
