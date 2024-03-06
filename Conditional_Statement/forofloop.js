cb506078a8fe0d58cb0275661188c007471ca0fd
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:33:06 +0530

    Create forofloop.js

---
+++ Conditional_Statement/forofloop.js
@@ -0,0 +1,18 @@
+//for of loop
+/*
+for (let val of str){
+    console.log(val);
+}*/
+//this is used for string or array to find the length and set of charcters
+let str = "Sujan"
+
+
+let size = 0;
+for (let val of str) { //iterator ->characters
+    console.log(val);
+    size++;
+}
+console.log(size);
+
+
+//to find the size of the character
