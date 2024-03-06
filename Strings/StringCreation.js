91b8610185d200fe97ac12c661c9f3db9a05ec7b
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:38:16 +0530

    Create StringCreation.js

---
+++ Strings/StringCreation.js
@@ -0,0 +1,11 @@
+//Strings - sequence of character  used to represent text
+//Strings Creation
+let str = "Sujanps";
+let str2 = 'sujanps';
+//Inbuilt property
+// string length
+// str.length
+//string indices-index
+console.log(str[0]); //S
+console.log(str[str.length - 1]); //s(last letter of string)
+console.log(str[4]); //n
