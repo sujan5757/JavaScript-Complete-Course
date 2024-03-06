fad178c6d06a5091bd93b8561debbcf095ac0342
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:50:29 +0530

    Create forEach.js

---
+++ Homework-Sums/forEach.js
@@ -0,0 +1,17 @@
+//for a given array of numbers print the square of each value using the for each loop
+
+
+var numbers = [1, 2, 3, 4, 5];
+
+
+for (var i = 0; i < numbers.length; i++) {
+    console.log(numbers[i] * numbers[i]);
+}
+
+
+let num = [1, 2, 3, 4, 5];
+
+
+num.forEach = (num) => {
+    console.log(num * num);
+}
