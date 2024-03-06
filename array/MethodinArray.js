d182fa2a3e902a8b40042d8a715612fc9955e41c
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:43:52 +0530

    Create MethodinArray.js

---
+++ array/MethodinArray.js
@@ -0,0 +1,11 @@
+//methods in array
+
+
+let city = ["delhi", "ckm", "bangalore"];
+// for (let cityy in city) {
+//     console.log(city);
+// }
+//uppercase function
+for (let cityy in city) {
+    console.log(city.toUpperCase());
+}
