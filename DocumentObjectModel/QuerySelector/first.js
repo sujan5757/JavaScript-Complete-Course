23f9f0aa1db88bf61a14c1d4292fd09cb9510d13
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:57:53 +0530

    Create first.js

---
+++ DocumentObjectModel/QuerySelector/first.js
@@ -0,0 +1,6 @@
+let h2 = document.querySelector("h2");
+console.dir(h2.innerText);
+h2.innerText = h2.innerText + "from apna College student"; //concatenate
+
+
+//create 3 div with common class name -"box".access them& add some unique text to each of them
