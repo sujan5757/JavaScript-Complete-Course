92a735e49d312c3138d5bf6fd360024c24c2861e
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:56:20 +0530

    Create QuerySelector.js

---
+++ DocumentObjectModel/QuerySelector.js
@@ -0,0 +1,17 @@
+//query selector
+
+
+let element = document.querySelector(selector);
+console.dir(firstElements)
+    //DOM MANIPULATION  document, t query, selector
+
+
+//PROPERTICES :
+/*
+tagName: returns tag for element nodes
+innerText: returns the text content of the element and all its children
+innerHtml: returns the plain text and HTML tags  contents in the element
+textContent : return textual content even for hidden elements
+
+
+*/
