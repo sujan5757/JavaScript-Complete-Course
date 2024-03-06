dd08b4ce940f90b207439fcb3dd2ceb8a95460ee
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:24:53 +0530

    Create Assignmenet-operator.js

---
+++ Operator/Assignmenet-operator.js
@@ -0,0 +1,10 @@
+//assignemnet operator
+let a = 7; //=equal to operator
+a += 1; //+= operator plus or equal
+console.log(a);
+let b = 6;
+b -= 1 //-= minus or qual to
+console.log(b);
+let c = 9;
+c *= 2; //star or equal operator
+console.log(c);
