72dc104823ecbfd8ea39bd9fb426b72be259a782
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:49:26 +0530

    Create ArrowFunction.js

---
+++ array/ArrowFunction.js
@@ -0,0 +1,17 @@
+//arrow function  compact way of writing a function
+//sum funtion
+
+
+// function sum(a, b) {
+//     return a + b;
+// }
+const x = (a, b) => {
+    console.log(a + b);
+};
+x(2, 3);
+//multiplication function
+
+
+function multiply(a, b) {
+    return a * b;
+}
