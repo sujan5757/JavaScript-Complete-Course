c1746da9fbc91b7669b32ae481dbcd4d31555083
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:43:00 +0530

    Create ArrayIndices.js

---
+++ array/ArrayIndices.js
@@ -0,0 +1,22 @@
+//array indices
+
+
+let arr = [87, 34, 67, 34, 67];
+
+
+console.log(arr[0]);
+
+
+console.log(arr[1]);
+
+
+console.log(arr[2]);
+
+
+console.log(arr[3]);
+
+
+arr[2] = 89; // to change the array value
+console.log(arr[2]);
+//we cant do it in string because strings are immutable thats y we cant do this in string
+//array is mutable
