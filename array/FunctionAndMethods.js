e34226780d02bc04ed978174c51047d1d1e95afc
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:49:07 +0530

    Create FunctionAndMethods.js

---
+++ array/FunctionAndMethods.js
@@ -0,0 +1,38 @@
+//FUNCTION IN JS- block of code that perform a specific task , can be invoked whenever needed
+
+
+
+
+console.log("hello");
+"abs".toUpperCase();
+[1, 2, 3].push(4);
+//here log , toUppercase , push all are functions that perform task
+//function definition
+
+
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 2)); //function call
+//function invocation
+
+
+
+
+function myfunction() { //function definition
+    console.log("i love js ");
+}
+myfunction();
+myfunction(); // to print multiple times
+
+
+// redundancy - list repeat
+
+
+function f(msg, n) { //parameter ->input //local variable
+    console.log(msg * n);
+}
+f("i love js", 100); //argument  function  //global variable
+
+
+//output noN not on numbers
