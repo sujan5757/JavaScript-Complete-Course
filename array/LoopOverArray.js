c72ffcbc5e9f42831e8a936ff2fa5219c75273e0
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:43:28 +0530

    Create LoopOverArray.js

---
+++ array/LoopOverArray.js
@@ -0,0 +1,18 @@
+//loop over an array  - print all elements of an array  
+
+
+let heros = ["supporting", "supporting1", "supporting2", "supporting3", "supporting4"];
+
+
+for (let i = 0; i < heros.length; i++) {
+    console.log(heros[i]);
+}
+//for of loop
+let heroes = ["supporting", "supporting11", "supporting2", "supporting3", "supporting4"];
+
+
+for (let i of heroes) {
+    console.log(heroes);
+
+
+}
