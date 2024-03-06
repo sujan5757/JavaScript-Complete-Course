db07a15fd87ab33ccdc8d13a425276cb7a4383cc
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:51:51 +0530

    Create ForEach.js

---
+++ array/ForEach.js
@@ -0,0 +1,20 @@
+//fpr each loop in array
+//Array.foreach(callBackfunction)
+// here it is an a function to executefor each element in the array
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+arr.forEach(function printValue(value) {
+    console.log(value);
+
+
+})
+
+
+//arr.forEach((value) => console.log(value.toUppercase());
+
+
+//high order function - the function which takes other functions as a parameter a
+
+
+//for each is only used in array s  not i   strings
