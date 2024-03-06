8ea12a2e77143708536ce6eb1071bc3994dd83be
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-06 09:40:06 +0530

    Create EscapeCharacter.js

---
+++ Strings/EscapeCharacter.js
@@ -0,0 +1,4 @@
+//escape character \t \r\n
+console.log("escape \n characters") //\nnext line\tnext tab
+let str = "apna\ncollege"
+console.log(str.length) //it will not display 13 because \n it will consider as a single character so it will display 12.
