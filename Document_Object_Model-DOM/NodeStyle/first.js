5209157c22ed60d9693782012142bb932cbfe11e
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-07 17:29:34 +0530

    Create first.js

---
+++ Document_Object_Model-DOM/NodeStyle/first.js
@@ -0,0 +1,7 @@
+//JS
+let div=document.querySelector("div")
+div.style.backgroundColor ="green";
+div.style.visibility="hidden";
+div.style.fontSize="26px";
+div.innerText="hello!"
+console.log("div");
