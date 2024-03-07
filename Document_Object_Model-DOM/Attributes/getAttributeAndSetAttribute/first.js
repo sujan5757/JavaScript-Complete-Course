cd22fd0d4cee3d0e0d852837a1640bc9f2f515f1
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2024-03-07 17:27:39 +0530

    Create first.js

---
+++ Document_Object_Model-DOM/Attributes/getAttributeAndSetAttribute/first.js
@@ -0,0 +1,20 @@
+//getAttribute
+let div=document.querySelector("div");
+console.log(div);
+let id= div.getAttribute("id");
+console.log(id);
+
+
+let name=div.getAttribute("name");
+console.log(name);
+// let para=document.querySelector("p");
+// console.log(para.getAttribute("class"));
+
+
+//setAttribute
+
+
+let para=document.querySelector("p");
+console.log(para.setAttribute("class","new Value"));
+
+
